25b5444a021ae5048bc22935d492590d8ef794c1
pranshi2300 <pranshigupta230@gmail.com>
2026-08-21 22:51:20 +0530

    Create 119-Pascal'sTriangleII

---
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.cpp
@@ -0,0 +1,13 @@
+// Last updated: 8/21/2026, 10:51:21 PM
+1class Solution {
+2public:
+3    vector<int> getRow(int rowIndex) {
+4        vector<int> row(rowIndex + 1,1);
+5        for(int i=1;i<rowIndex;i++){
+6            for(int j=i;j>=1;j--){
+7                row[j]=row[j]+row[j-1];
+8            }
+9        }
+10        return row;
+11    }
+12};
